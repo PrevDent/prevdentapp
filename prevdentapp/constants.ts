2237cb32a025b0b18408f0eae242ca9c7c7586c1
Vitor Santos <vitorsacz@gmail.com>
2025-05-13 04:47:40 -0300

    adicionando url para testes locais

--- prevdentapp/constants.ts
+++ prevdentapp/constants.ts
@@ -1,2 +1,3 @@
 export const URL_PREVDENT_API = "http://10.0.2.2:5001";
+export const URL_PREVDENT_LOCAL_JAVA_API = "http://10.0.2.2:8080";
 export const URL_PREVDENT_JAVA_API = "https://prevdent-back-java.azurewebsites.net";
