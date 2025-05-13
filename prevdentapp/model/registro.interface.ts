c068fa1839ac85bab887fc46ecc4b9a210188854
Vitor Santos <vitorsacz@gmail.com>
2025-05-13 15:10:53 -0300

    criando interface de novo registro

--- prevdentapp/model/registro.interface.ts
+++ prevdentapp/model/registro.interface.ts
@@ -1,7 +1,8 @@
 export interface RegistroInterface{
     idRegistro?: string;
+    icon?: string
     tipo: string;
     ocorrencia: string;
-    intensidade: string;
+    intensidade: number | string;
     informacoes_adicionais: string;
 }
